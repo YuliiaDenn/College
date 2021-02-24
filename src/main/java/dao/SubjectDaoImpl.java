58ea0d638e645bc2653750706eb1c1f45cedafcb
YuliiaDenn <bnk4722@gmail.com>
2021-02-24 14:46:52 +0200

    Delete Domain

--- src/main/java/dao/SubjectDaoImpl.java
+++ src/main/java/dao/SubjectDaoImpl.java
@@ -3,7 +3,6 @@ package dao;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.SQLIntegrityConstraintViolationException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashSet;
